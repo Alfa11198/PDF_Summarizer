ac9d8b953561cc5579c90d1e692c2765eedb2534
Alfa11198 <alfadelta11198@gmail.com>
2023-12-13 22:29:22 +0530

    update

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-print("Starting {0} app........".format(os.getenv('APP1_NAME')))
+print("Starting {0} app........".format(os.getenv("APP1_NAME")))
 # print(os.getenv('OPENAI_API_KEY'))
-OpenAI.api_key = os.getenv('OPENAI_API_KEY')
+OpenAI.api_key = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI()
 
@@ -27,11 +27,11 @@ def pdf_txt_extract(file):
 
     text_obj["text"] = txt
     text_obj["pages"] = len(pdf.pages)
-    text_obj["title"] = (pdf.metadata.title)
-    text_obj["subject"] = (pdf.metadata.subject)
-    text_obj["bytesizeinmemory"] = (pdf.metadata.__sizeof__())
-    text_obj["authors"] = (pdf.metadata.author)
-    text_obj["creators"] = (pdf.metadata.creator)
+    text_obj["title"] = pdf.metadata.title
+    text_obj["subject"] = pdf.metadata.subject
+    text_obj["bytesizeinmemory"] = pdf.metadata.__sizeof__()
+    text_obj["authors"] = pdf.metadata.author
+    text_obj["creators"] = pdf.metadata.creator
 
     return text_obj
 
@@ -45,22 +45,19 @@ def summ_text(txt, max_tokens=150, temp=0.7, p=1, n=1):
                 messages=[
                     {
                         "role": "system",
-                        "content": "Summarize content you are provided with."
+                        "content": "Summarize content you are provided with.",
                     },
-                    {
-                        "role": "user",
-                        "content": txt
-                    }
+                    {"role": "user", "content": txt},
                 ],
                 temperature=temp,
                 max_tokens=max_tokens,
                 top_p=p,
-                n=n
+                n=n,
             )
-        except openai.OpenAIError as e:
-            st.warning(
-                f"OpenAI Rate limit exceeded. Waiting for 60 seconds. Error: {e}")
-            time.sleep(60)
+        except Exception as e:
+            st.warning(f"OpenAI Rate limit exceeded. Waiting for 60 seconds. Error: {e}")
+            # time.sleep(60)
+            exit(1)
     # print(response.choices[0].message.content)
     return response.choices[0].message.content
 
@@ -70,49 +67,34 @@ def chunks(text, max_tokens=4096):
     chunks = []
     for j in range(len(text)):
         # print(len(text[j]), type(text[j]))
-        chunk = [text[j][i:i + max_tokens]
-                 for i in range(0, len(text[j]), max_tokens)]
+        chunk = [text[j][i : i + max_tokens] for i in range(0, len(text[j]), max_tokens)]
         chunks += chunk
     # print(len(chunks), type(chunks), type(chunks[0]))
     return chunks
 
-def ui_app():
 
+def ui_app():
     # Stramlit Basic Page UI
-    st.set_page_config(page_title="PDF Summ")
-    st.title("PDF Summarizer App")
+    st.set_page_config(page_title=os.getenv("APP1_NAME"))
+    st.title(f"{os.getenv('APP1_NAME')} App")
     st.caption("Powered by OpenAI & Streamlit")
     st.caption("Created by Abhishek Dey")
 
-    open_ai_key = st.text_input(label="Enter Your Open AI Key", type="password", placeholder="Give Open AI API Key")
-    free_personal_key = st.toggle(label="Personal Free API Key", value=True)
-    p_free_api_key = None
-
     # Capture the Input PDF File
-    file = st.file_uploader(label="Upload Your PDF Here", type=[
-                            'pdf'], accept_multiple_files=False)
+    file = st.file_uploader(label="Upload Your PDF Here", type=["pdf"], accept_multiple_files=False)
 
     # Variable Slidera
-    max_tk_val = st.slider("Select Max. Output Token :",
-                           min_value=10, max_value=200, value=100)
-    temp_val = st.slider("Select Temperature :",
-                         min_value=0.1, max_value=2.0, value=0.7)
+    max_tk_val = st.slider("Select Max. Output Token :", min_value=10, max_value=200, value=100)
+    temp_val = st.slider("Select Temperature :", min_value=0.1, max_value=2.0, value=0.7)
     top_p_val = st.slider("Select Top P :", min_value=1, max_value=3, value=1)
     n_val = st.slider("Select N :", min_value=1, max_value=3, value=1)
 
     # Onclick the Summarize Button
     if st.button(label="Summarize"):
-
-        if open_ai_key:
-            OpenAI.api_key = open_ai_key
-            p_free_api_key = free_personal_key
-
         # checks whether the file is available
         if file is not None:
-
             # Load Spinner
             with st.spinner(text="Generating Summary..."):
-
                 # capture all the variables value
                 p_max_tk = max_tk_val
                 p_temp = temp_val
@@ -134,17 +116,17 @@ def ui_app():
                 st.subheader("Summary: \n")
 
                 for i, t_chunk in enumerate(text_chunks):
-
                     # Generate summary
-                    summ_txt_val = summ_text(
-                        txt=t_chunk, temp=p_temp, max_tokens=p_max_tk, p=p_top_p, n=p_n)
+                    summ_txt_val = summ_text(txt=t_chunk, temp=p_temp, max_tokens=p_max_tk, p=p_top_p, n=p_n)
 
                     # Print Summary of the Chunk
-                    st.caption(summ_txt_val+"\n")
+                    st.caption(summ_txt_val + "\n")
 
                     # Waiting for the next chunk to mitigate Open AI rate limit
-                    if (((i+1) < len(text_chunks)) and p_free_api_key):
+                    if (i + 1) < len(text_chunks):
                         time.sleep(30)
+                    else:
+                        time.sleep(15)
 
             # print("Completed All Chuncks")
 
